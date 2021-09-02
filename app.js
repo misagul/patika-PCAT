7c463ec1edaa986c0f69c6c40e61499839c6a6bf
M. Isa Gul <isagul7890@gmail.com>
2021-09-02 14:42:14 +0300

    add photo post method created, middlewares added

--- app.js
+++ app.js
@@ -2,11 +2,30 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+//ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.post('/photos', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
